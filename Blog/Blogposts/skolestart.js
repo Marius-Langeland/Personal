1b88b8b83478170d3efb0fcfa26b2a24923bf48f
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-09-06 15:10:03 +0200

    Updated links

--- Blog/Blogposts/skolestart.js
+++ Blog/Blogposts/skolestart.js
@@ -1,7 +1,8 @@
 $(function(){
-    $("#søknader>span").click(function(){
+    $(".collapsed").click(function(){
         $(this).toggleClass("expanded");
         if(!$(this).hasClass("expanded"))
             this.scrollIntoView({behavior: "smooth", block: "center"});
     });
+    $(".extension-indicator").html("<dot></dot><dot></dot><dot></dot>")
 });
