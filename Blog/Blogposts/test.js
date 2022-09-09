c779ea6a384b31c4eba09a7042e5c7c9b7da403b
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-09-09 14:10:32 +0200

    Postrefactor

--- Blog/Blogposts/test.js
+++ Blog/Blogposts/test.js
@@ -4,28 +4,29 @@ let size = 0;
 
 
 function scroll(delta){
-    target += delta;
-    target = target>size? size : target<0? 0 : target;
-    if(current != Math.floor(target)){
-        current = Math.floor(target);
-        $('.target').addClass('fade');
-        $(`.page:nth-of-type(${current+2})`).addClass("target");
-    }
+    if(delta != undefined)
+    {
+        target += delta;
+        target = target>size? size : target<0? 0 : target;
+        if(current != Math.floor(target)){
+            let c = Math.sign(current - target) == -1 ? 'above' : 'below';
+            current = Math.floor(target);
+            $('.target').addClass(c).removeClass('target');
+            let obj = $(`.page:nth-of-type(${current+2})`);
+            $(obj).removeClass('above below')
+            $(obj).addClass("target");
 
-    $('.progress').css('height', `${target / size * 100}%`)
+            let clr = $(obj).data('color');
+            if(clr != undefined)
+                $('body').css('background-color', clr);
+        }
+
+        $('.progress').css('height', `${target / size * 100}%`)
+    }
 }
 
 $(function(){
     size = $('.page').length;
     scroll(0);
-    $(window).bind('mousewheel DOMMouseScroll', function(e){
-        scroll(e.originalEvent.detail / 100);
-    });
-
-    $('.page').on('transitionend webkitTransitionEnd oTransitionEnd', function(){
-        let obj = $('.fade');
-        obj.addClass('reset');
-        obj.removeClass('target fade');
-        obj.removeClass('reset');
-    });
+    addEventListener('wheel', (e) => scroll(e.deltaY / 1000));
 });
