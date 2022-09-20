d6ccdd3a084b36b974cc63c4abbf9a7cbdb35dd3
Marius <mariuslangeland.ml@gmail.com>
2022-09-20 18:53:05 +0200

    Removed Company class, company is now an implicit concept

--- Marximulator/objects.ts
+++ Marximulator/objects.ts
@@ -1,29 +1,45 @@
-let companyTemplate = 
-`<section class="company">
-    <div class="company-name">Company name</div>
-    <div class="directors"></div>
-    <div class="productive-workers"></div>
-    <div class="unproductive-workers"></div>
-</section>`;
+function getCompanyTemplate(name: string){
+    return  `<section class="company" id="${name}">
+                <div class="company-name">Company name</div>
+                <div class="directors"></div>
+                <div class="productive-workers"></div>
+                <div class="unproductive-workers"></div>
+            </section>`;
+}
 
-let personTemplate =
-`<div class="person">
-    <span class="name">Example name</span>
-    <span class="value"></span>
-</div>`;
+function getPersonTemplate(name: string){
+    return  `<div class="person" id="${name}">
+                <span class="name">Example name</span>
+                <span class="value"></span>
+            </div>`;
+}
 
 class Person{
+    constructor(name){
+        this.name = name;
+    }
+    
     public name: string;
     public age: number;
     public skill: number;
     public efficiency: number;
-    public shifts: number[];
+
+    public employedAt: string[];
+    public values: string[];
+    public titles: string[];
+    public shiftStart: number[];
+    public shiftEnd: number[];
 }
 
-class Company{
-    public directors: Person[];
-    public workers: Person[];
-    public value: number;
+let people: Person[] = [];
+let hour = 0;
+
+function hireWorker(person: Person, company: string, titles: string, ...shifts: number[]){
+    let existingCompany = document.getElementById(company);
+    if(existingCompany == undefined){
+        let newCompanyHTML = getCompanyTemplate(company);
+        document.body.append(newCompanyHTML);
+    }
 }
 
-document.body.append(companyTemplate);
+hireWorker(new Person("Marius"), "Pharma", "Director");
