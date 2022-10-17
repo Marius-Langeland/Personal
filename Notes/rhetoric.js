2ae63802a2cd582b6480ff76794ab95b4f8af2d1
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-10-17 13:33:09 +0200

    Added scrolling highlight feature

--- Notes/rhetoric.js
+++ Notes/rhetoric.js
@@ -1,13 +1,25 @@
 window.onload = function(){
 
+    let manualToggle = false;
     // Toggle the light on the cards
     let terms = document.getElementsByClassName('term');
     for (let i = 0; i < terms.length; i++) {
             const e = terms[i];
         e.addEventListener('click', () => {
             e.classList.toggle('open');
+            manualToggle = true;
         });
     }
 
-    
+    let cards = document.querySelectorAll('.term');
+    let current = -1;
+    setInterval(function(){
+        if(!manualToggle){
+            if(current != -1)
+                cards[current].classList.remove('open');
+            
+            current = (current + 1) % cards.length;
+            cards[current].classList.add('open');
+        }
+    }, 2000);
 }
